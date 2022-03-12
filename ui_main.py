197559639a027ef41e07a09e555aa1ee74a0d169
andrade33 <andrade33.john@gmail.com>
2022-03-11 22:34:14 -0300

    corrigindo nome da ui

--- ui_main.py
+++ ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'QT_06.ui'
+## Form generated from reading UI file 'tela.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.2
 ##
