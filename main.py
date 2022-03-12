214a6bc925c0ef973c4cae6c324e14937e39e1ba
andrade33 <andrade33.john@gmail.com>
2022-03-11 22:26:21 -0300

    Comitando

---
+++ main.py
@@ -0,0 +1,34 @@
+
+from PySide6.QtCore import QPropertyAnimation, QEasingCurve
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_main import Ui_MainWindow
+import sys
+
+class RegApp(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(RegApp, self).__init__(parent=parent)
+        self.setupUi(self)
+
+        self.toggleBtn.clicked.connect(lambda: self.openLeftTab_menu())
+
+
+    def openLeftTab_menu(self):
+        
+        width = self.left.width()
+        if width == 0:
+            newWid = 200;
+        else:
+            newWid = 0
+
+        self.animation = QPropertyAnimation(self.left, b"maximumWidth")
+        self.animation.setDuration(500)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(newWid)
+        self.animation.setEasingCurve(QEasingCurve.InOutQuart)
+        self.animation.start() 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = RegApp()
+    window.show()
+    sys.exit(app.exec())
---
+++ ui_main.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'QT_06.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.left = QFrame(self.centralwidget)
+        self.left.setObjectName(u"left")
+        self.left.setMaximumSize(QSize(0, 16777215))
+        self.left.setStyleSheet(u"background-color: rgb(0, 170, 0);")
+        self.left.setFrameShape(QFrame.StyledPanel)
+        self.left.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.left)
+
+        self.main = QFrame(self.centralwidget)
+        self.main.setObjectName(u"main")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.main.sizePolicy().hasHeightForWidth())
+        self.main.setSizePolicy(sizePolicy)
+        self.main.setStyleSheet(u"background-color: rgb(148, 255, 148);")
+        self.main.setFrameShape(QFrame.StyledPanel)
+        self.main.setFrameShadow(QFrame.Raised)
+        self.toggleBtn = QPushButton(self.main)
+        self.toggleBtn.setObjectName(u"toggleBtn")
+        self.toggleBtn.setGeometry(QRect(90, 40, 75, 24))
+        self.toggleBtn.setStyleSheet(u"background-color: rgb(255, 85, 0);")
+
+        self.horizontalLayout.addWidget(self.main)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.toggleBtn.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+
